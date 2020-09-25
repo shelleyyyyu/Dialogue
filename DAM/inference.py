52fc29e9aa7c1571910ebcc2addbf2c3545e8501
shelleyyyyu <menghsuanyu26@gmail.com>
2020-09-25 09:52:06 +0800

    add inference method

---
+++ DAM/inference.py
@@ -0,0 +1,58 @@
+import sys
+import os
+import time
+
+import cPickle as pickle
+import tensorflow as tf
+import numpy as np
+
+import utils.reader as reader
+import models.net as net
+import utils.evaluation as eva
+#for douban
+#import utils.douban_evaluation as eva
+
+import bin.train_and_evaluate as train
+import bin.test_and_evaluate as test
+
+# configure
+
+conf = {
+    "data_path": "./data/douban/data.pkl",
+    "save_path": "./output/douban/",
+    "word_emb_init": "./data/douban/word_embedding_debug.pkl",
+    "init_model": './output/douban/model.ckpt.10', #should be set for test
+
+    "rand_seed": None, 
+
+    "drop_dense": None,
+    "drop_attention": None,
+
+    "is_mask": True,
+    "is_layer_norm": True,
+    "is_positional": False,  
+
+    "stack_num": 5,  
+    "attention_type": "dot",
+
+    "learning_rate": 1e-3,
+    "vocab_size": 256358, #ubuntu: 434512; jdqa: 256358; douban: 172130
+    "batch_size": 32, #200 for test
+    "emb_size": 80,
+
+    "max_turn_num": 1,
+    "max_turn_len": 30,
+
+    "max_to_keep": 1,
+    "num_scan_data": 2,
+    "_EOS_": 1, #1 for douban data 28270 for ubuntu
+    "final_n_class": 1,
+}
+
+
+model = net.Net(conf)
+#train.train(conf, model)
+
+#test and evaluation, init_model in conf should be set
+test.test(conf, model)
+
